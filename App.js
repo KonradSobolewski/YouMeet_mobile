7c410d9af901c1d25761acbaca7e0812a8b1355b
Konrad Sobolewski <konradsob12@gmail.com>
2018-11-17 20:30:33 +0100

    refactor

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {StyleSheet, View} from 'react-native';
 import Home from './src/components/HomeMap/Home'
 import Login from './src/components/LoginPages/Login'
 import Register from './src/components/RegistrationPages/reigster'
-import {createMaterialTopTabNavigator, createStackNavigator} from 'react-navigation'
+import {createMaterialTopTabNavigator, StackNavigator} from 'react-navigation'
 import {Font} from "expo";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -15,46 +15,48 @@ export default class App extends React.Component {
     }
 }
 
-const MainNav = createStackNavigator({
-    loginPage: {
-        screen: Login
-    },
-    homePage: {
-        screen: Home
-},
-});
-
-
 const AppStackNavigator = createMaterialTopTabNavigator({
-    loginPage: {
-        screen: MainNav,
-        navigationOptions: {
-          tabBarLabel: 'Login',
-          showIcon: true,
-          tabBarIcon: () => {
-            return <Ionicons name="md-contact" size={20} color={"white"} />
-          }
+        loginPage: {
+            screen: StackNavigator({
+                    loginPage: {
+                        screen: Login
+                    },
+                    homePage: {
+                        screen: Home
+                    }
+                },
+                {
+                    navigationOptions: {
+                        header: null
+                    }
+                }),
+            navigationOptions: {
+                tabBarLabel: 'Login',
+                showIcon: true,
+                tabBarIcon: () => {
+                    return <Ionicons name="md-contact" size={20} color={"white"}/>
+                }
+            }
+        },
+        registerPage: {
+            screen: Register,
+            navigationOptions: {
+                tabBarLabel: 'Register',
+                showIcon: true,
+                tabBarIcon: () => {
+                    return <Ionicons name="md-create" size={20} color={"white"}/>
+                }
+            }
         }
     },
-    registerPage: {
-        screen: Register,
-        navigationOptions: {
-          tabBarLabel: 'Register',
-          showIcon: true,
-          tabBarIcon: () => {
-            return <Ionicons name="md-create" size={20} color={"white"} />
-          }
-        }
-    }
-  },
     {
-    tabBarOptions: {
-      showLabel: true, // hide labels
-      showIcon: true,
-      style: {
-          backgroundColor: '#9C27B0' // TabBar background
-      }
-    }
+        tabBarOptions: {
+            showLabel: true, // hide labels
+            showIcon: true,
+            style: {
+                backgroundColor: '#9C27B0' // TabBar background
+            }
+        }
     }
 );
 const styles = StyleSheet.create({
