98c373518aade0dae589ab161df98728d25d1c41
Konrad Sobolewski <konradsob12@gmail.com>
2018-11-18 23:38:02 +0100

    sign out

--- src/components/HomeMap/Home.js
+++ src/components/HomeMap/Home.js
@@ -3,6 +3,7 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import FetchLocation from "./FetchLocation";
 import UsersMap from "./UsersMap";
 import UserInfo from "./UserInfo";
+import {onSignOut} from '../../config/authorization'
 
 export default class Home extends React.Component {
 
@@ -33,6 +34,12 @@ export default class Home extends React.Component {
         }, err => console.log(err));
     };
 
+    signOut = ()=> {
+        onSignOut()
+            .then(() => this.props.navigation.navigate('loginPage'))
+            .catch(err => console.log(err));
+    };
+
     render() {
         const {navigation} = this.props;
         if (this.state.auth === null) {
@@ -43,6 +50,9 @@ export default class Home extends React.Component {
                 <UserInfo onLoad={this.state.userInfo}/>
                 <FetchLocation onGetLocation={this.getUserLocationHandler}/>
                 <UsersMap userLocation={this.state.location} userInfo={this.state.userInfo}/>
+                <TouchableOpacity style={styles.buttonContainer} onPress={this.signOut}>
+                    <Text style={styles.buttonText}>Logout</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -54,5 +64,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    buttonContainer: {
+        borderRadius: 15,
+        marginTop: 15,
+        backgroundColor: 'rgba(0,255,255,1)',
+        padding: 15
+    },
+    buttonText: {
+        color: 'black',
+        textAlign: 'center'
     }
 });
