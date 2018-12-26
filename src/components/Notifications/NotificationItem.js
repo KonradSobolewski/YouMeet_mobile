c71690cbb4371693ec41b9e33840f3089b84984a
Walczakp007 <walczakp007@gmail.com>
2018-12-26 18:45:55 +0100

    Change style of notification item

--- src/components/Notifications/NotificationItem.js
+++ src/components/Notifications/NotificationItem.js
@@ -4,14 +4,22 @@ import {getMetUserIcon} from "../../services/user.service";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const notificationItem = props => {
-    let date = new Date(Date.parse(props.historyData.params.startDate));
-    console.log('chuj');
+    let date = new Date(Date.parse(props.historyData.params.creationDate));
+    let peopleWaiting = props.historyData.params.joinerId.length == 1 ? '1 person waiting!' : props.historyData.params.joinerId.length + ' people waiting!';
     return(
         <View style={styles.container}>
-                <Text style={styles.person}>{props.historyData.meeting_id}</Text>
-                <TouchableOpacity onPress={() => props.acceptMeeting(props.historyData.meeting_id, props.historyData.params.joinerId[0])}>
-                    <Ionicons name="md-add-circle" size={30} color={'#373D3F'} style={styles.icon}/>
-                </TouchableOpacity>
+            <TouchableOpacity style={styles.userIcon} onPress={() => props.acceptMeeting(props.historyData.meeting_id, props.historyData.params.joinerId[0])}>
+                <Ionicons name="md-add-circle" size={30} color={'#373D3F'} style={styles.icon}/>
+            </TouchableOpacity>
+            <View style={styles.rightBox}>
+                <Text style={styles.person}>{peopleWaiting}</Text>
+                <Text style={styles.description}>{props.historyData.params.description}</Text>
+            </View>
+            <View style={styles.leftBox}>
+                <Text style={styles.date}>{date.toDateString() + ' ' + date.getHours() + ':' + date.getMinutes()}</Text>
+                <Text style={styles.description}>{props.historyData.params.placeDescription.replace(/(\r\n\t|\n|\r\t)/gm,' ')}</Text>
+            </View>
+
         </View>
     );
 };
